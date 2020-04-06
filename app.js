27916acadd59d838283a0e3676b8abaa7919844a
JuliaAbud <abud.julia@gmail.com>
2020-04-06 12:20:03 -0300

    deploy

---
+++ app.js
@@ -0,0 +1,26 @@
+let TwitterPackage = require('twitter');
+let credentials = require('./credentials.json');
+
+let secret = {
+  consumer_key: credentials.apiConsumerKey,
+  consumer_secret: credentials.apiConsumerSecret,
+  access_token_key: credentials.accessToken,
+  access_token_secret: credentials.accessSecret
+}
+
+var params = {
+  q: 'brunoav',
+  count: 10
+}
+
+let Twitter = new TwitterPackage(secret);
+
+Twitter.get('users/search', params, function(err, data, res) {
+    let fer = data[0].id_str;
+    Twitter.get('statuses/user_timeline', {id: fer}, function(err, tweets, res) {
+       tweets.forEach((tweet) => {
+          Twitter.post('favorites/create', {id: tweet.id_str});
+       })
+    });
+
+});
